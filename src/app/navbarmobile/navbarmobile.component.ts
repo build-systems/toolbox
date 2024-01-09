50aa8d4ef3f36e5705b40b8a6b4a5c38923ac3da
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-01-09 15:46:54 +0100

    Logo opacity 1, 2px wide. + Navbarmobile component

---
+++ src/app/navbarmobile/navbarmobile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbarmobile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbarmobile.component.html',
+  styleUrl: './navbarmobile.component.css'
+})
+export class NavbarmobileComponent {
+
+}
