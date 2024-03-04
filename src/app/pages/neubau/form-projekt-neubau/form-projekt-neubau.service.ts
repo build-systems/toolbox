caa76dce5587d93b33b409fc768fd760efc7a6c2
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-03-04 21:38:37 +0100

    Radio legend hidden, added div with title with tooltip icon on the side

--- src/app/pages/neubau/form-projekt-neubau/form-projekt-neubau.service.ts
+++ src/app/pages/neubau/form-projekt-neubau/form-projekt-neubau.service.ts
@@ -172,7 +172,7 @@ export class FormProjektNeubauService {
     min: 0,
     max: 1000,
     step: 1,
-    title: 'Grundstücksbezogene Kosten (KG 100, 200) [€/m²]',
+    title: 'Grundstücksbezogene Kosten [€/m²]',
     disabled: false,
   };
 
@@ -182,7 +182,7 @@ export class FormProjektNeubauService {
     min: 0,
     max: 100,
     step: 1,
-    title: 'Baunebenkosten (KG 700) [%]',
+    title: 'Baunebenkosten [%]',
     // title: 'Baunebenkosten ohne Finanzierungskosten [%]',
     disabled: false,
   };
