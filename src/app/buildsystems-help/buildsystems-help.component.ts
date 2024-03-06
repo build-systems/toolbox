a5926923a4811603d05adac747fe5fd4c2ccaf24
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-03-06 11:08:17 +0100

    Added animation to Hilfe button

--- src/app/buildsystems-help/buildsystems-help.component.ts
+++ src/app/buildsystems-help/buildsystems-help.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-buildsystems-help',
   standalone: true,
   imports: [],
   templateUrl: './buildsystems-help.component.html',
-  styleUrl: './buildsystems-help.component.css'
+  styleUrl: './buildsystems-help.component.css',
 })
 export class BuildsystemsHelpComponent {
   isCopied: boolean = false;
 
-  async copyContent(){
-    try{
+  async copyContent() {
+    try {
       await navigator.clipboard.writeText('mail@buildsystems.de');
-      this.isCopied = true
-      setTimeout(()=> {
-        this.isCopied = false}, 1500)} catch (e) {
-        console.error('e', e)
-      }
+      this.isCopied = true;
+      setTimeout(() => {
+        this.isCopied = false;
+      }, 1500);
+    } catch (e) {
+      console.error('e', e);
+    }
   }
-
 }
