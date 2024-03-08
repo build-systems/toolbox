cb77ca2162bb1202b1fd8da9adab336acfada631
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-03-08 23:30:11 +0100

    Started landing page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,7 +8,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   host: {
-    class: 'homepage'
+    class: 'home'
   }
 })
 export class HomeComponent {
