7e7627cf5962373199af05603dea1f2263df7580
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-04-18 19:43:37 +0200

    Deleted the radio button component imports

--- src/app/pages/sanierung/form-projekt-sanierung/form-projekt-sanierung.component.ts
+++ src/app/pages/sanierung/form-projekt-sanierung/form-projekt-sanierung.component.ts
@@ -4,12 +4,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormProjektSanierungService } from './form-projekt-sanierung.service';
 import { TooltipDirective } from '../../../shared/tooltip.directive';
 import { allgemein, sanierung } from '../../../shared/tooltips';
-import { RadioButtonsComponent } from '../../../radio-buttons/radio-buttons.component';
 
 @Component({
   selector: 'app-form-projekt-sanierung',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RadioButtonsComponent, TooltipDirective],
+  imports: [CommonModule, ReactiveFormsModule, TooltipDirective],
   templateUrl: './form-projekt-sanierung.component.html',
   styleUrl: './form-projekt-sanierung.component.css',
   host: {
