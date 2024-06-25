5143d4c73ec8ee2ae35676e3058dec27c79583af
daniel-locatelli <daniel.nlocatelli@gmail.com>
2024-06-25 20:36:45 +0200

    minor changes to portfolio

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SupabaseService } from '../../shared/supabase.service';
+import { AuthComponent } from '../../auth/auth.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AuthComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css',
   host: {
-    class: 'development'
-  }
+    class: 'host-portfolio',
+  },
 })
 export class PortfolioComponent {
+  session = this.supabase.session;
 
+  constructor(private readonly supabase: SupabaseService) {}
+
+  ngOnInit() {
+    console.log(this.session);
+  }
 }
